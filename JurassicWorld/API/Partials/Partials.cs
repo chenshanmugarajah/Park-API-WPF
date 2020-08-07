fbe215c0e64762accee6ee361c06ac2fd76028ae
chen shanmugarajah <chenshanmugarajah@gmail.com>
2020-08-07 15:41:08 +0100

    added base files

---
+++ JurassicWorld/API/Partials/Partials.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public partial class Park
+    {
+        public override string ToString()
+        {
+            return $"Park: {ParkName} with capacity {ParkCapacity}";
+        }
+    }
+    public partial class Animal
+    {
+        public override string ToString()
+        {
+            return $"Animal: {AnimalName} weighing {AnimalWeightTons} tons";
+        }
+
+    }
+}
